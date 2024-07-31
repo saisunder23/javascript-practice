6b7ddb11aaa3cc4aa243e83a7acbe2ab997093ec
Sunder Sai <saisunder23@gmail.com>
2024-07-30 21:39:57 -0400

    workshop practice

---
+++ script.js
@@ -0,0 +1,43 @@
+// // 1) Pseudocode: instructions for toothbrush: 
+
+// console.log("Grab a toothbrush with your hand.")
+
+// // 2) Declare variable for number of cows: 
+
+// let numCows = 50 ; 
+
+// // 3) Create string that says Hello World 
+
+// let greeting = "hello, World!";
+
+// // 4) Hello world in console log 
+
+// console.log("Hello World");
+
+
+// // 5) Concatenate strings "hello" and "disney land!" in single line of Java script
+
+// console.log("Hello," + "Disney Land");
+
+// // 6) Create a variable for the following with the arithmetic operator: the number of umbrellas is ten times three. 
+
+// let umbrellas = 10*3;
+
+
+// // 7) Create a variable called num using the addition operator to add five and one. Afterward, display the value using console.log.
+
+// num = 5 + 1;
+// console.log(num);
+
+// // 8) Add an external script file to an HTML file.
+
+// // 9) Concatenate three strings (1) Hello (2) [space] (3) World.
+
+// let three_strings = 'Hello' + " " + "World"; 
+
+// 10) Concatenate strings using variables (1) Hello, (2) [space] (3) I am (4) [space] (5) Josh. Display over a dialog box
+
+let greeting = 'Hello'; 
+let existence = 'I am'; 
+let name = 'Sunder'; 
+alert(greeting + " " + existence + " " + name);
